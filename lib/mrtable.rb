1a221ce887668fb8964da8e4c8b06213f7bb53fe
sonota88 <yosiot8753@gmail.com>
2017-10-29 09:50:39 +0900

    Delete require "mrtable/version"

--- lib/mrtable.rb
+++ lib/mrtable.rb
@@ -1,6 +1,5 @@
 # coding: utf-8
 
-require "mrtable/version"
 require "json"
 
 module Mrtable
--- test/mrtable_test.rb
+++ test/mrtable_test.rb
@@ -1,10 +1,6 @@
 require 'test_helper'
 
 class MrtableTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Mrtable::VERSION
-  end
-
   def test_serealize_col
     assert_equal '" \\| "', Mrtable.serealize_col(" | ")
   end
